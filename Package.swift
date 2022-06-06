3dbda8c1815186b6639ea0246224ee7c0ac36e7f
anton_anisimov <anton_anisimov@epam.com>
2022-06-06 11:15:43 +0300

    q

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .target(
             name: "ARVideoKit",
-            dependencies: [],
+            dependencies: ["HaishinKit"],
             path: "ARVideoKit")
     ]
 )
