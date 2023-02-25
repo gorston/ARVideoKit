878d0c887f0f2b2a73922507632fbcf86dc74a9c
anton_anisimov <anton_anisimov@epam.com>
2023-02-25 09:56:32 +0300

    test

--- ARVideoKit/Rendering/RenderAR.swift
+++ ARVideoKit/Rendering/RenderAR.swift
@@ -117,7 +117,7 @@ struct RenderAR {
             let size = UIScreen.main.bounds.size
             var renderedFrame: UIImage?
             pixelsQueue.sync {
-                renderedFrame = renderEngine.snapshot(atTime: self.time, with: size, antialiasingMode: .none)
+                renderedFrame = renderEngine.snapshot(atTime: self.time, with: size, antialiasingMode: .none).rotate(by: -90)
             }
             if let _ = renderedFrame {
             } else {
